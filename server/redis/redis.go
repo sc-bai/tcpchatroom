74d77a80aa545c2821e8701fa2f817dd23268676
sc.bai_t <sc.baiT@shunwang.com>
2022-07-09 22:24:56 +0800

    redis 存储修改

--- server/redis/redis.go
+++ server/redis/redis.go
@@ -3,13 +3,15 @@ package rediscache
 // redis 采用hash 把clientuser 存储起来
 import (
 	"chatroom/comm"
+	"encoding/json"
 	"fmt"
-	"reflect"
 
 	"github.com/go-redis/redis"
 )
 
 var DBclient *redis.Client
+
+var g_userkey string = "chat_users"
 
 type RedisDb struct {
 }
@@ -28,25 +30,8 @@ func InitDb() error {
 
 func (p *RedisDb) PutUser(user comm.LoginMessage) error {
 
-	// 单个设置
-	/* _, err := p.DBclient.HSet(user.UserId, "username", user.UserName).Result()
-	if err != nil {
-		fmt.Printf("sb hset username error: %v\n", err)
-		return err
-	}
-	_, err = p.DBclient.HSet(user.UserId, "userpasswd", user.UserPasswd).Result()
-	if err != nil {
-		fmt.Printf("sb hset userpasswd error: %v\n", err)
-		return err
-	}
-	*/
-
-	userinfo := make(map[string]interface{})
-	userinfo["userid"] = user.UserId
-	userinfo["username"] = user.UserName
-	userinfo["userpasswd"] = user.UserPasswd
-
-	s, err := DBclient.HMSet(user.UserName, userinfo).Result()
+	b, _ := json.Marshal(user)
+	s, err := DBclient.HSet(g_userkey, user.UserName, string(b)).Result()
 	if err != nil {
 		fmt.Printf("err: %v\n", err)
 		return err
@@ -56,42 +41,23 @@ func (p *RedisDb) PutUser(user comm.LoginMessage) error {
 }
 
 func (p *RedisDb) DelUser(username string) {
-
-	// 不用差了 直接删
-	/* 	i, err := p.DBclient.HMGet(username, "userid", "username", "userpasswd").Result()
-	   	if err != redis.Nil || i == nil {
-	   		fmt.Printf("err: %v\n", err)
-	   		return err // 不存在
-	   	} */
-	DBclient.HDel(username).Result()
+	DBclient.HDel(g_userkey, username).Result()
 }
 
 func (p *RedisDb) FindUser(username string) comm.LoginMessage {
 	var ret comm.LoginMessage
 
-	i, err := DBclient.HMGet(username, "userid", "username", "userpasswd").Result()
+	s, err2 := DBclient.HGet(g_userkey, username).Result()
+	if err2 != nil {
+		fmt.Printf("[server] redis hget error: %v\n", err2)
+		return ret
+	}
+
+	err := json.Unmarshal([]byte(s), &ret)
 	if err != nil {
-		fmt.Printf("DBclient.HMGet: %v\n", err)
-
-		if err == redis.Nil {
-			fmt.Println("HmGet redis.nil")
-		}
+		fmt.Println("redis: finduser json.Unmarshal error,", err.Error())
 		return ret
 	}
-
-	if i[0] == nil {
-		return ret
-	}
-
-	fmt.Printf("[server]: DBclient.HMGet result: %v\n", i)
-	if len(i) != 3 {
-		return ret
-	}
-
-	fmt.Println(" dbclient finduser success..")
-	ret.UserId = reflect.ValueOf(i[0]).String()
-	ret.UserName = reflect.ValueOf(i[1]).String()
-	ret.UserPasswd = reflect.ValueOf(i[2]).String()
 
 	return ret
 }
